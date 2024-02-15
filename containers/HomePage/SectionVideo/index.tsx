2a503e2cb9e24900e98c60092bbc67b9b8987454
SaldanhaElisson <elissonsaldanha19@gmail.com>
2024-02-15 20:58:53 -0300

    get cord in videos with currenttime

--- containers/HomePage/SectionVideo/index.tsx
+++ containers/HomePage/SectionVideo/index.tsx
@@ -1,9 +1,44 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 
 export const SectionVideo = () => {
+  const [x, setX] = useState<Number>(0);
+  const [y, setY] = useState<Number>(0);
+  let timeCurrent: number = 0;
+
   return (
-    <section className='h-full w-full rounded-2xl bg-surfaceWhite'>
-      <div className='h-96 w-56'></div>
+    <section className='flex h-full w-full items-center justify-center rounded-2xl bg-surfaceWhite'>
+      <div>
+        <video
+          controls
+          id='video'
+          width='1000'
+          muted
+          height='600'
+          preload='auto'
+          onMouseMove={(event) => {
+            const video = document.getElementById('video') as HTMLVideoElement;
+
+            timeCurrent = video?.currentTime;
+            console.log(video?.currentTime);
+
+            const targe = event.target as HTMLElement;
+
+            const propsElement = targe.getBoundingClientRect();
+
+            const x = event.clientX - propsElement.left;
+            const y = propsElement.bottom - event.clientY;
+
+            setY(y);
+            setX(x);
+          }}
+        >
+          <source src='video3.mp4' type='video/mp4' />
+        </video>
+
+        <div className='mt-9 text-4xl'>{`Time: ${timeCurrent} Y: ${y} X: ${x}`}</div>
+      </div>
     </section>
   );
 };
