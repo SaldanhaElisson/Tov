24a6d4d24e5eaba25c6cb09885ca7c0dd99c2a1e
SaldanhaElisson <elissonsaldanha19@gmail.com>
2024-03-14 19:14:17 -0300

    feat(tov02): remove comments

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,8 @@ import { HeaderSection } from '@/containers/login-page/header-section';
 import { GoogleSection } from '@/containers/login-page/google-section';
 
 export default function LoginPage() {
-  // const user = await getUserSession();
-
   return (
-    <main className='bg-login-page-lg  flex h-screen  w-screen  justify-center bg-cover'>
+    <main className='flex  h-screen w-screen  justify-center  bg-login-page-lg bg-cover'>
       <section className=' mt-[10%] flex h-fit w-fit flex-col items-center gap-36'>
         <HeaderSection />
 
